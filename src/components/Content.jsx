6d3a7bae815dbe98c6491e878aefeb93d50b489d
satyawikananda <satyawikananda456@gmail.com>
2020-07-21 00:48:23 +0800

    :zap: Change logo

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,28 +1,26 @@
 import React, {Fragment, useEffect, useState} from "react"
-// import Card from "./card/Card"
-import Modal from "./modal/Modal"
-import logo from "../logo.svg"
+import logo from "../assets/img/pokeact.png"
 import axios from "axios"
 
 const Content = () => {
     const url = "https://pokeapi.co/api/v2/pokemon/"
     const countPokemon = 400
-    // eslint-disable-next-line no-unused-vars
     const [data, setData] = useState([])
-    const [modal, isModalOpen] = useState(false)
-    const [dataOne, setOneData] = useState({ data:[] })
+    const [loading, isLoading] = useState(false)
 
-    const getSpecific = async(id) => {
-        const getData = await axios(`${url}${id}`);
-        const result = getData.data
-        return result
+    function capitalizeWord(word) {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase()
     }
     
     useEffect(() => {
         const fetchData = async (id) => {
+            isLoading(true)
+
             const getData = await axios(`${url}${id}`);
             const result = getData.data
             setData(data => [...data, result])
+            
+            isLoading(false)
         };
 
         for (let i = 1; i <= countPokemon; i++) {
@@ -34,27 +32,23 @@ const Content = () => {
     return(
         <Fragment>
             <div className="flex flex-col text-center w-full mb-20">
-                <img src={logo} alt="Logo" className="w-24 h-24 mx-auto text-white bg-indigo-500 rounded-full"/>
+                <img src={logo} alt="Logo" className="w-24 h-24 mx-auto text-white bg-indigo-500 rounded-full App-logo"/>
                 <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Find Pokemon in Pokeact</h1>
                 <p className="lg:w-2/3 mx-auto leading-relaxed text-base">A Pokemon list made with library React JS</p>
             </div>
             <div className="flex flex-wrap justify-center">
-                {data.map(item => (
-                    <div className="xl:w-1/6 md:w-full sm:w-full p-4 m-2 bg-gray-300" key={item.id}>
-                        <img className="h-40 rounded w-full object-cover object-center mb-6 bg-white" src={item.sprites.front_default} alt={item.name} />
-                        <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">#{item.id}</h3>
-                        <h2 className="text-lg font-medium title-font mb-4 text-gray-700">{item.name}</h2>
-                        <button className="flex mx-auto mt-2 text-white bg-indigo-500 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-lg" onClick={() => {
-                            isModalOpen(true);
-                            setOneData(getSpecific(item.id))
-                            }}>Detail</button>
-                    </div>
-                ))}
+                {loading ? (<h1>Loading...</h1>) : (
+                    <Fragment>
+                        {data.map(item => (
+                            <div className="xl:w-1/6 md:w-full sm:w-full p-4 m-2 bg-gray-300" id="list" key={item.id}>
+                                <img className="h-40 rounded w-full object-cover object-center mb-6 bg-white" src={item.sprites.front_default} alt={item.name} />
+                                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">#{item.id}</h3>
+                                <h2 className="text-lg font-medium title-font mb-4 text-gray-700">{capitalizeWord(item.name)}</h2>
+                            </div>
+                        ))}
+                    </Fragment>
+                )}
             </div>
-            {/* {modal ? (
-                <Modal />
-                ) : null 
-            } */}
         </Fragment>
     )
 }
--- src/components/master/Footer.jsx
+++ src/components/master/Footer.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react"
-import logo from "../../logo.svg"
+import logo from "../../assets/img/pokeact.png"
 
 const Footer = () => {
     return (
